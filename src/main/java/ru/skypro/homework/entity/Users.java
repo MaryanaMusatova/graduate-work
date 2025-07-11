a67e572cace6dc9c2f3240c548a8034cda184223
Artem Vasyatkin <dustler98@gmail.com>
2025-07-11 22:47:00 +0300

    Добавил сущность объектов

---
+++ src/main/java/ru/skypro/homework/entity/Users.java
@@ -0,0 +1,23 @@
+package ru.skypro.homework.entity;
+
+import lombok.*;
+import com.example.exampleforgraduatework.dto.Role;
+
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+//этот класс станет entity
+public class Users {
+    private Integer id;
+    private String email;
+    private String image;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Role role;
+}
